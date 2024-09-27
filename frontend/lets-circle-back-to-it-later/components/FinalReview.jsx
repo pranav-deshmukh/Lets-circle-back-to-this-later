9868de9cc81d335235c60b6384c45043194008fd
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-09-28 01:04:52 +0530

    pros and cons

--- frontend/lets-circle-back-to-it-later/components/FinalReview.jsx
+++ frontend/lets-circle-back-to-it-later/components/FinalReview.jsx
@@ -1,14 +1,34 @@
+import Footer from "./Footer";
+import ProsAndCons from "./ProsAndCons";
 import ReviewParams from "./ReviewParams";
 import { data } from "@/sample/sampledata";
 
-const {pros, cons, ...filteredData} = data
+const { pros, cons, ...filteredData } = data;
 
 function FinalReview() {
   return (
-    <div className="w-full h-full min-h-[300px] md:grid items-center justify-center grid-cols-2 place-items-center gap-y-10 gap-x-4 md:p-10 flex flex-col">
-      {Object.entries(filteredData).map(([key,value],idx) => {
-        return <ReviewParams key={idx} title={key} value={value}/>;
-      })}
+    <div className="w-full h-full flex flex-col">
+      <div className="md:grid items-center justify-center grid-cols-2 place-items-center gap-y-10 gap-x-4 md:p-10 flex flex-col">
+        {Object.entries(filteredData).map(([key, value], idx) => {
+          return <ReviewParams key={idx} title={key} value={value} />;
+        })}
+      </div>
+      <div className="flex justify-evenly items-center">
+        <div className="">
+            <p>Pros</p>
+          {Object.entries(pros).map(([key, value], idx) => {
+            return <ProsAndCons key={idx} title={key} value={value} />;
+          })}
+        </div>
+        <div className="border-l-[2px] border-black h-[300px]"></div>
+        <div className="pb-10">
+            <p>Cons</p>
+          {Object.entries(cons).map(([key, value], idx) => {
+            return <ProsAndCons key={idx} title={key} value={value} />;
+          })}
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
--- frontend/lets-circle-back-to-it-later/components/ReviewParams.jsx
+++ frontend/lets-circle-back-to-it-later/components/ReviewParams.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 function ReviewParams({ title, value }) {
   return (
-    <div className="w-full max-w-[650px] md:h-[300px] p-4 bg-[#F5F5DC] overflow-y-auto">
-      <div className="font-bold text-lg mb-2">{title}</div>
-      <div>{value!="None"?value:"No data available"}</div>
+    <div
+      className={`w-full max-w-[650px] md:h-[250px] p-4 bg-[#F5F5DC] overflow-y-auto ${
+        value == "None" ? "hidden" : "flex flex-col"
+      }`}
+    >
+      <div className="flex justify-end">
+        <Image src={"/SampleImg.png"} height={150} width={150} alt="sample" />
+      </div>
+      <span className="font-bold text-lg mb-2">{title}</span>
+      <span>{value}</span>
     </div>
   );
 }
