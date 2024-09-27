7e3f6f2a5d3fcdf559b60a5d9bdc8e9f549623de
Omansh Sharma <74657563+arduR-O@users.noreply.github.com>
2024-09-27 23:46:33 +0530

    feat: LLM functionality added

--- backend/helpers/LLM/classify.py
+++ backend/helpers/LLM/classify.py
@@ -1,14 +1,21 @@
 import json
+import os 
+from makeRequest import makeRequest
 
-def classify(details : dict):
+def classify(product: dict):
     """
     input: object containing the name and about
     output: category of the product
     """
-    prompt = "based on the given name and details of the electronic product classify it into one of the following categories: 'smartphones', 'laptops', 'tablets', 'smartwatches', 'televisions', 'gaming consoles', 'digital cameras', 'bluetooth speakers', 'e-readers', 'smart home devices', 'refrigerators', 'washing machines', 'dishwashers', 'microwave ovens', 'air conditioners'\nreturn it form of a JSON like this: {'category': 'smartphones'}\nthe reply should be stricly json object and nothing else:\n"
-    prompt += str(details)
-    print(prompt)
-    return name_about
+    details = {"product-name": product["product-name"], "about": product["about"]}
+    SysPrompt = "based on the given name and details of the electronic product classify it into one of the following categories: 'smartphone', 'laptop', 'tablet', 'smartwatch', 'television', 'gaming console', 'digital camera', 'bluetooth speaker', 'e-reader', 'smart home device', 'refrigerator', 'washing machine', 'dishwasher', 'microwave oven', 'air conditioner'\nreturn it form of a JSON like this: {'category': 'smartphones'}\nthe reply should be stricly json object and nothing else:\n"
+    userMsg = str(details)
+    models = ["gemma2-9b-it","gemma-7b-it"]
+    category = json.loads(makeRequest(SysPrompt, userMsg, models))
+    try:
+        return category["category"]
+    except:
+        return None
 
 if __name__ == '__main__':
     products = None
@@ -17,5 +24,5 @@ if __name__ == '__main__':
         products = json.load(f)
     with open("./parameters.json") as f:
         keys = json.load(f).keys()
-    name_about = {products[0]["product-name"], products[0]["about"]}
-    classify(name_about)
+    # name_about = {products[0]["product-name"], products[0]["about"]}
+    print(classify(products[0]))
