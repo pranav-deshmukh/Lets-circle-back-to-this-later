991f8f54bf29b0db8a90ca6d923ca076297948d1
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-09-27 22:24:38 +0530

    feat: review product nav

--- frontend/lets-circle-back-to-it-later/components/ReviewPageNav.jsx
+++ frontend/lets-circle-back-to-it-later/components/ReviewPageNav.jsx
@@ -1,4 +1,5 @@
 import { Roboto } from "next/font/google";
+import { Input } from "./ui/input";
 
 const Robo = Roboto({
   weight: "700",
@@ -11,10 +12,14 @@ function ReviewPageNavigation() {
       <div className="w-[10%] ml-4">
         <span className="md:flex hidden">Reviews.com</span>
       </div>
-      <div className="w-[70%] flex justify-center gap-20">
+      <div className="w-[60%] flex justify-center gap-20">
         <span>Home</span>
         <span>About</span>
         <span>Contact</span>
+      </div>
+      <div className="w-[30%] flex justify-center items-center">
+
+        <Input className="max-w-[300px] h-[40px] bg-white border-2 border-black rounded-full" placeholder="Search Product..."/>
       </div>
     </div>
   );
