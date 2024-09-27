4e417790af0838c22e9cf88469e7beb469b1a8ff
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-09-28 00:17:26 +0530

    review data display fixed

--- frontend/lets-circle-back-to-it-later/components/FinalReview.jsx
+++ frontend/lets-circle-back-to-it-later/components/FinalReview.jsx
@@ -1,12 +1,13 @@
 import ReviewParams from "./ReviewParams";
+import { data } from "@/sample/sampledata";
 
-const arr = ["", "", "", "", "", "", "", "", "", "", ""];
+const {pros, cons, ...filteredData} = data
 
 function FinalReview() {
   return (
     <div className="w-full h-full min-h-[300px] md:grid items-center justify-center grid-cols-2 place-items-center gap-y-10 gap-x-4 md:p-10 flex flex-col">
-      {arr.map((idx) => {
-        return <ReviewParams key={idx} />;
+      {Object.entries(filteredData).map(([key,value],idx) => {
+        return <ReviewParams key={idx} title={key} value={value}/>;
       })}
     </div>
   );
--- frontend/lets-circle-back-to-it-later/components/ReviewParams.jsx
+++ frontend/lets-circle-back-to-it-later/components/ReviewParams.jsx
@@ -1,14 +1,10 @@
-function ReviewParams() {
-    return (
-        <div className="w-full max-w-[650px] md:h-[500px] h-[300px] p-4 bg-[#F5F5DC] overflow-y-auto">
-            <div>
-                ReviewParams
-            </div>
-            <div className="">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea vitae distinctio rerum enim quod excepturi aliquid dolorum, ratione odio labore voluptatem aut? Animi, iste vitae! Nemo repellendus error perferendis, fugit cum eum dolores vitae culpa non quibusdam voluptatem quae tempora illum minus accusamus, hic, quisquam itaque provident maxime totam? Odio quis perferendis numquam, ea praesentium quisquam cum autem magnam, earum animi quaerat amet maxime voluptatem ullam nulla, ipsam harum vero eius et adipisci tenetur sunt unde maiores est? Eum soluta nesciunt numquam quasi tempore, facilis inventore a neque rem earum possimus iure sed blanditiis maxime tenetur. Totam debitis nemo aut?
-            </div>
-        </div>
-    );
+function ReviewParams({ title, value }) {
+  return (
+    <div className="w-full max-w-[650px] md:h-[300px] p-4 bg-[#F5F5DC] overflow-y-auto">
+      <div className="font-bold text-lg mb-2">{title}</div>
+      <div>{value!="None"?value:"No data available"}</div>
+    </div>
+  );
 }
 
 export default ReviewParams;
