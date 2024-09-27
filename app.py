43baed8514edd89a292db7d3ba95508751ab36ea
Arv2040 <aravcr4085@gmail.com>
2024-09-27 12:49:48 +0530

    made changes

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
+
+
+app.route('/info',method = ['get'])
+def scrape(url):
+    print("hello world")
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
